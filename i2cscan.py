0b76900c00f7325a70384250f0effcb8b60600a4
Abhik Chowdhury <Abhik.ch6@gmail.com>
2018-06-18 19:02:10 -0700

    added outline for scan

--- i2cscan.py
+++ i2cscan.py
@@ -1,22 +1,29 @@
-#entity name
-#entity ty
-
-
-#
 
 
 import time
+millis = int(round(time.time() * 1000))
+
 starttime=time.time()
 while True:
   print "tick"
   time.sleep(60.0 - ((time.time() - starttime) % 60.0))
 
-entety_name = ""
-entity_type = ""
-entity_uuid = ""
+entety_name = "Abhik"
+entity_type = "Human"
+entity_uuid = "31415926"
 
-sensor_set = ""
+sensor_set = "AbhiksSmartPostureCorrectorTM"#AbhiksCasualLeftHandGlove
 
-sensing_units = []
+#the manufacturer provides documentation regarding the position, purpose and function of each of sensors
+imported_sensing_units = [["MPU60500", "AbhiksBeautifulBreathingSensor","MAX30100",....]]
 
-sensing_
+imported_sensor_schemas = [["MPU60500",i2c, 0x68, 64, [ACCELL_Y, [REG_N, REG_N+1],[GYRO_Z, [REG_P, REG_P+1]],....]],
+                            ["AbhiksBeautifulBreathingSensor", i2c, 0x11, 128, [VAL, [RED_N, REG_N+1]]],...],
+                            ["MPU60501",...],
+                            ["ExcitingECG"],
+
+
+#create person floder
+#create sensor set folder
+#iterate over sensing units and make folders for each, then look it up in schemas and then make folders for every data stream
+#start collecting data
